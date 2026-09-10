1d07407944ae8ab4632d7e79d5b5fd591b10dc64
ewefwefwfwf1 <noooooooooooooooooooooo@atomicmail.io>
2026-09-10 22:59:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1339,7 +1339,7 @@ async def handle_create_command(text: str):
     if len(parts) < 2:
         return L("create_format")
     label = parts[1]
-    if not re.match(r'^[a-zA-Z0-9\-_. \u0600-\u06FF\u200c\s]+$', label, re.UNICODE,):
+    if not re.match(r'^[a-zA-Z0-9\-_. \u0600-\u06FF\u200c\u200d\u2600-\u27BF\uFE0F\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FAFF\U0001F1E6-\U0001F1FF\s]+$', label, re.UNICODE,):
         return L("create_bad_name")
 
     limit_value = 0.0
