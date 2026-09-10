32746a9ef24363ddd6569c8e9c8544629be353a9
ewefwefwfwf1 <noooooooooooooooooooooo@atomicmail.io>
2026-09-10 16:51:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3813,7 +3813,7 @@ body[dir="rtl"]{direction:rtl;text-align:right}
 
   <!-- MAIN CONTENT -->
   <main class="main">        <div style="text-align:center;padding:0 0 10px 0;">
-      <img src="/client/logo.png" alt="logo" style="max-width:440px;width:100%;height:auto;filter:drop-shadow(0 0 20px rgba(59,130,246,0.5));">
+            <img src="/client/logo.png" alt="logo" style="max-width:470px;width:100%;height:auto;filter:drop-shadow(0 0 20px rgba(59,130,246,0.5));">
     </div>
 
     <!-- Dashboard -->
